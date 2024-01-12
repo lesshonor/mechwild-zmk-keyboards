af7738feda3d8f4d1aa1041ba54d4324bfa4ed73
honorless <86894501+lesshonor@users.noreply.github.com>
2024-01-12 10:24:09 -0500

    feat: zephyr 3.5 upgrade

--- boards/arm/pillbug/board.cmake
+++ boards/arm/pillbug/board.cmake
@@ -3,6 +3,7 @@
 set(OPENOCD_NRF5_SUBFAMILY nrf52)
 board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
 
+include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd-nrf5.board.cmake)
